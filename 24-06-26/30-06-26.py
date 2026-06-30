1981ef38bcf2433436472503751c83415e048b6b
Ali Akbar <alixakbrr231@gmail.com>
2026-06-30 11:20:41 +0500

    second Commit

--- 24-06-26/30-06-26.py
+++ 24-06-26/30-06-26.py
@@ -5,3 +5,19 @@ b = 'ali'
 print(b)
 a = "hi"=="world"
 print(type(a))
+
+a =160
+b=a/60
+print(b)
+
+x = 20+30
+y = x-40
+print(y)
+
+var1 = 5-55 
+var2 = var1/10
+print(var2)
+
+var3 = 3+2*2
+var4 = (3+2)*2
+var5 = var3+var4
